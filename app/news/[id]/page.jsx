ac1672515d587c7883aff61258f5e185fb847099
Reema Thakur <reemathakur2114@gmail.com>
2024-05-10 11:38:42 +0530

    routing, layout, not found

---
+++ app/news/[id]/page.jsx
@@ -0,0 +1,24 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+
+const NewsDetail = ({ params }) => {
+  const newsId = params.id;
+  const newsItem = DUMMY_NEWS.find((news) => news?.slug === newsId);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem?.image}`} alt={newsItem?.title} />
+        <h1>{newsItem?.title}</h1>
+        <time dateTime={newsItem?.date}>{newsItem?.date}</time>
+      </header>
+      <p>{newsItem?.content}</p>
+    </article>
+  );
+};
+
+export default NewsDetail;
